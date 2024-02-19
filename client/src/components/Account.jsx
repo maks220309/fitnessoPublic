748a00b927efcec53c363689841375bdb5c6f404
maks220309 <maks.chabounia@gmail.com>
2024-02-19 22:59:10 +0300

    addAccountPage

--- client/src/components/Account.jsx
+++ client/src/components/Account.jsx
@@ -1,10 +1,23 @@
 import styles from "../styles/Account.module.css";
-import LeftPart from "./LeftPart";
 
 function Account() {
   return (
     <>
-        <div className={styles.accountContainer}></div>
+      <div className={styles.accountContainer}>
+        <div className={styles.blockAccount}>
+          <span>Account</span>
+        </div>
+        <div className={styles.accountList}>
+          <div className={styles.blockWithAva}>
+            <img className={styles.ava} src="https://pbs.twimg.com/profile_images/1701878932176351232/AlNU3WTK_400x400.jpg"></img>
+            <span>Amelia Watson</span>
+          </div>
+          <div className={styles.buttons}>
+            <div className={styles.changeAva}>Change Avatar</div>
+            <div className={styles.changeNick}>Change Nickname</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
